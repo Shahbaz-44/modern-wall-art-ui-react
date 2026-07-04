236b10fc096173cafb465f9c9742cc44138bf663
Shahbaz Haider <alibhaialiali774@gmail.com>
2026-07-04 20:05:46 +0500

    Initial Commit

---
+++ src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,52 @@
+import "./Navbar.css"
+import { useState } from "react";
+import logo from '../../assets/Images/Logo.png'
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import { FaInstagram } from "react-icons/fa";
+
+const Navbar = () => {
+   // false = menu band
+  // true = menu open
+    const [isOpen, setIsOpen ] = useState(false);
+
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <div className="navbar-content">
+
+            <div className="logo">
+                <img src={logo} alt="Logo" />
+            </div>
+
+            <div className="menu">
+                { isOpen?(
+                <RiCloseLine
+                  className="menu-icon"
+                  onClick={() => setIsOpen (false)} />) : (
+                <RiMenu3Line
+                   className="menu-icon cross-icon"
+                   onClick={() => setIsOpen (true)} />
+                )}
+                <FaInstagram className="insta-icon" />
+            </div>
+             
+        </div>
+        {/* mobile menu */}
+        { isOpen && (
+        <div className="menu-items">
+            <ul>
+                <li>Sectors</li>
+                <li>How It Works</li>
+                <li>Gallery</li>
+                <li>Start Your Own Business</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar
